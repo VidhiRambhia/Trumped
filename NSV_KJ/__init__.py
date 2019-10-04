c8fddc658157b68e8864e41b2977571a7a6b2111
Nidhee Kamble <38733826+nidheekamble@users.noreply.github.com>
2019-10-04 11:53:00 +0530

    __init__.py done

---
+++ NSV_KJ/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+#from flask_app import app as application
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Central.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+from NSV_KJ import routes
