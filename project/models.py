fe90aa22da27adbb83bc01a4d62278c1bdc1d32f
shrey-c <shreyansh.chheda@gmail.com>
2019-10-05 16:44:01 +0530

    models updated

--- project/models.py
+++ project/models.py
@@ -5,13 +5,26 @@ from project import login_manager, db
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
-class User(db.Model,UserMixin):
+class Publisher(db.Model,UserMixin):
 	__tablename__ = 'user'
 	id = db.Column(db.Integer, primary_key = True)
 	email = db.Column(db.String(30),unique=True,nullable = False)
 	name = db.Column(db.String(30), nullable = False)
 	password = db.Column(db.String(128), unique = False, nullable = False)
-	photo = db.Column(db.String(20), unique = False, default = 'default.jpg' , nullable= True )
+	address = db.Column(db.String(500), unique= True, nullable = False)
+
 
 	def __repr__(self):
 		return f"User('{self.id}', {self.name}', '{self.email}')"
+
+class Posts(object):
+	"""docstring for Posts"""
+	__tablename__ = 'post'
+	id = db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String(1000), nullable = False)
+	text = db.Column(db.String(3000),unique=True,nullable = False)
+	author = db.Column(db.String(128), unique = False, nullable = False)
+	type_of_post = db.Column(db.String(500), unique= True, nullable = False)
+	real = db.Column(db.Integer, nullable=False)
+	user_id = db.relationship("User_id", back_poulates = "user")
+		
