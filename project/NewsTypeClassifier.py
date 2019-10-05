6de30a10dcce9c17f2b40130962071362ab8d62c
shrey-c <shreyansh.chheda@gmail.com>
2019-10-06 00:39:09 +0530

    making news type model work

--- project/NewsTypeClassifier.py
+++ project/NewsTypeClassifier.py
@@ -11,13 +11,17 @@ with open('news', 'r') as f:
     text = f.read()
     news = text.split("\n\n")
     count = {'sport': 0, 'world': 0, "us": 0, "business": 0, "health": 0, "entertainment": 0, "sci_tech": 0}
+    counter=0
     for news_item in news:
+        counter = counter +1
+        if(counter>1433):
+            break
         lines = news_item.split("\n")
-        print(lines[6])
         file_to_write = open('data/' + lines[6] + '/' + str(count[lines[6]]) + '.txt', 'w+')
         count[lines[6]] = count[lines[6]] + 1
         file_to_write.write(news_item)  # python will convert \n to os.linesep
         file_to_write.close()
+
 
 
 
@@ -80,7 +84,7 @@ docs_new = "Messi joins other football team"
 docs_new = [docs_new]
 
 #LOAD MODEL
-loaded_vec = CountVectorizer(vocabulary=pickle.load(open("count_vector.pkl", "rb")))
+loaded_vec = CountVectorizer(vocabulary=pickle.load(open("count_vector_type.pkl", "rb")))
 loaded_tfidf = pickle.load(open("tfidf_type.pkl","rb"))
 loaded_model = pickle.load(open("nb_model_type.pkl","rb"))
 
