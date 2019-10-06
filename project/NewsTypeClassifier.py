70b5cc4cbd8c6b5ed62574d3505007de5c51536e
shrey-c <39230642+shrey-c@users.noreply.github.com>
2019-10-06 07:19:54 +0530

    Added lost file

---
+++ project/NewsTypeClassifier.py
@@ -0,0 +1,93 @@
+import pandas
+import glob
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+
+
+with open('news', 'r') as f:
+    text = f.read()
+    news = text.split("\n\n")
+    #print(news)
+    count = {'sport': 0, 'world': 0, "us": 0, "business": 0, "health": 0, "entertainment": 0, "sci_tech": 0}
+    counter=0
+    for news_item in news:
+        lines = news_item.split("\n")
+        file_to_write = open('data/' + lines[6] + '/' + str(count[lines[6]]) + '.txt', 'w+')
+        count[lines[6]] = count[lines[6]] + 1
+        file_to_write.write(news_item)  # python will convert \n to os.linesep
+        file_to_write.close()
+
+
+
+
+category_list = ["sport", "world", "us", "business", "health", "entertainment", "sci_tech"]
+directory_list = ["data/sport/*.txt", "data/world/*.txt","data/us/*.txt","data/business/*.txt","data/health/*.txt","data/entertainment/*.txt","data/sci_tech/*.txt",]
+
+text_files = list(map(lambda x: glob.glob(x), directory_list))
+text_files = [item for sublist in text_files for item in sublist]
+
+training_data = []
+
+
+for t in text_files:
+    f = open(t, 'r')
+    f = f.read()
+    t = f.split('\n')
+    training_data.append({'data' : t[0] + ' ' + t[1], 'flag' : category_list.index(t[6])})
+    
+training_data[0]
+
+training_data = pandas.DataFrame(training_data, columns=['data', 'flag'])
+training_data.to_csv("train_data_type.csv", sep=',', encoding='utf-8')
+print(training_data.data.shape)
+
+
+
+
+#GET VECTOR COUNT
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(training_data.data)
+
+#SAVE WORD VECTOR
+pickle.dump(count_vect.vocabulary_, open("count_vector_type.pkl","wb"))
+
+
+
+#TRANSFORM WORD VECTOR TO TF IDF
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+#SAVE TF-IDF
+pickle.dump(tfidf_transformer, open("tfidf_type.pkl","wb"))
+
+# Multinomial Naive Bayes
+
+
+
+#clf = MultinomialNB().fit(X_train_tfidf, training_data.flag)
+X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, training_data.flag, test_size=0.25, random_state=42)
+clf = MultinomialNB().fit(X_train, y_train)
+
+#SAVE MODEL
+pickle.dump(clf, open("nb_model_type.pkl", "wb"))
+
+
+
+category_list = ["sport", "world", "us", "business", "health", "entertainment", "sci_tech"]
+
+docs_new = "ouyvoiboibiou"
+docs_new = [docs_new]
+
+#LOAD MODEL
+loaded_vec = CountVectorizer(vocabulary=pickle.load(open("count_vector_type.pkl", "rb")))
+loaded_tfidf = pickle.load(open("tfidf_type.pkl","rb"))
+loaded_model = pickle.load(open("nb_model_type.pkl","rb"))
+
+X_new_counts = loaded_vec.transform(docs_new)
+X_new_tfidf = loaded_tfidf.transform(X_new_counts)
+predicted = loaded_model.predict(X_new_tfidf)
+
+print(category_list[predicted[0]])
